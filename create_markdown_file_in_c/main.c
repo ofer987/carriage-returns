e8b48093a818824f20331da81e4c4cccb3ba12ff
Dan Jakob Ofer <dan@ofer.to>
2024-09-08 13:27:17 -0400

    chore(c-app): refactor

--- create_markdown_file_in_c/main.c
+++ create_markdown_file_in_c/main.c
@@ -9,38 +9,24 @@ static char CARRIAGE_RETURN = 13;
 
 FILE *open_file(char *file_name, char *mode);
 void close_file(FILE *fptr);
+void write(char *message, int size, FILE *fptr);
 
 int main(int argc, char **argv)
 {
   FILE *markdown_file = open_file(file_name, "wb");
 
   char *message = "Hello World";
-  int size = 0;
-  if ((size = fwrite(message, strlen(message), 1, markdown_file)) == 0)
-  {
-    printf("Error writing to file\n");
 
-    exit(1);
-  }
-  if ((size = fwrite(&NEWLINE, sizeof(char), 1, markdown_file)) == 0)
-  {
-    printf("Error writing to file\n");
-
-    exit(1);
-  }
-  if ((size = fwrite(&CARRIAGE_RETURN, sizeof(char), 1, markdown_file)) == 0)
-  {
-    printf("Error writing to file\n");
-
-    exit(1);
-  }
+  fwrite(message, strlen(message), 1, markdown_file);
+  fwrite(&NEWLINE, sizeof(char), 1, markdown_file);
+  fwrite(&CARRIAGE_RETURN, sizeof(char), 1, markdown_file);
 
   close_file(markdown_file);
 
   FILE *read_markdown_file = open_file(file_name, "rb");
 
   char character_code;
-  size = 0;
+  int size = 0;
   while ((size = fread(&character_code, sizeof(char), 1, markdown_file)) != 0)
   {
     printf("Character is:\t%c.\tCode is:\t%d.\n", character_code, character_code);
@@ -69,6 +55,16 @@ void close_file(FILE *fptr)
   if (fclose(fptr) != 0)
   {
     printf("Error closing file\n");
+
+    exit(1);
+  }
+}
+
+void write(char *message, int size, FILE *fptr)
+{
+  if (fwrite(message, size, 1, fptr) == 0)
+  {
+    printf("Error writing to file\n");
 
     exit(1);
   }
